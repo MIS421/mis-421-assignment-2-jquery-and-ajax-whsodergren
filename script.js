8e16326a4e885678244f9f470b7befbb4a028f11
whsodergren <whsodergren@crimson.ua.edu>
2024-02-14 15:06:49 -0600

    test1

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 var len;
 var results = '';
+let backgroundImages = ["https://images.unsplash.com/photo-1683009427666-340595e57e43?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxfHx8ZW58MHx8fHx8",
+    "https://images.unsplash.com/photo-1505533321630-975218a5f66f?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8ZnJlZSUyMGltYWdlc3xlbnwwfHwwfHx8MA%3D%3D",
+    "https://images.unsplash.com/photo-1530076886461-ce58ea8abe24?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8ZnJlZSUyMGltYWdlc3xlbnwwfHwwfHx8MA%3D%3D"];
+console.log(backgroundImages);
 
 function apiSearch() {
   var params = {
@@ -10,9 +14,9 @@ function apiSearch() {
   };
 
   $.ajax({
-      url: 'my-api-url' + $.param(params),
+      url: 'https://api.bing.microsoft.com//v7.0/search?' + $.param(params),
       beforeSend: function (xhrObj) {
-        xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key", "my-api-key");
+          xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key", "4cfb30d5ced240b1b1f9b8b14f682b95");
       },
       type: "GET",
     })
@@ -28,4 +32,25 @@ function apiSearch() {
     .fail(function () {
       alert("error");
     });
+}
+
+function searchFunction() {
+    apiSearch();
+}
+
+function changeBackground() {
+    let randomIndex = Math.floor(Math.random() * backgroundImages.length);
+    document.body.style.backgroundImage = "url('" + backgroundImages[randomIndex] + "')";
+}
+
+function getCurrentTime() {
+    var currentTime = new Date();
+    var hours = currentTime.getHours();
+    var minutes = currentTime.getMinutes();
+
+    var formattedTime = (hours < 10 ? "0" : "") + hours + ":" + (minutes < 10 ? "0" : "") + minutes;
+
+    $('#time').text(formattedTime);
+
+    $('#time').dialog();
 }
